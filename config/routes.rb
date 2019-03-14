6e90319e89f61139518020416b671aa68f5ca10b
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-03-14 14:58:45 -0400

    User sign up works correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'about' => 'welcom#about'
+  get 'blocitoff' => 'welcome#indes'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
