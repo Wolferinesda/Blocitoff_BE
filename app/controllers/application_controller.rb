32c897e75015b7e3f601fc8d9f75ee7c50cb3ce7
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-03-19 17:44:12 -0400

    Adding users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :except => [:index, :about]
 
+  def after_sign_in_path_for(resource_or_scope)
+    current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
+  resources :users, only: [:show]
   get 'about' => 'welcome#about'
   get 'blocitoff' => 'welcome#index'
+  get 'users/show'
   root 'welcome#index'
 end
