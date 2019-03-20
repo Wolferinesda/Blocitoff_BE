384c97f6463de8a4250cb940561a27deaa736970
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-03-20 18:35:30 -0400

    Can add to do items to the to do list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get 'users/show'
+  get 'items/create'
+
   devise_for :users
-  resources :users, only: [:show]
+
+  resources :users, only: [:show] do
+    resource :items, only: [:create]
+  end
+  
   get 'about' => 'welcome#about'
   get 'blocitoff' => 'welcome#index'
   get 'users/show'
