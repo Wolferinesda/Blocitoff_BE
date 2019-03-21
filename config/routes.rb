348463220a69f6fb981c30f45140539c30b825a7
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-03-20 21:13:07 -0400

    Merging routing changes from user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'users/show'
   get 'items/create'
 
-=======
->>>>>>> user_profiles
   devise_for :users
 
   resources :users, only: [:show] do
     resource :items, only: [:create]
   end
-  
+
   get 'about' => 'welcome#about'
   get 'blocitoff' => 'welcome#index'
   get 'users/show' => 'users#show'
