3effc69194e194862bc4255a298d366f2132a15e
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-04-03 18:41:04 -0400

    Fixed distance of time

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,7 +1,7 @@
 module ItemsHelper
 
   def days_left(item)
-    distance_of_time_in_words(Time.now, item.created_at)
+    distance_of_time_in_words(Time.now, item.created_at - 7.days)
   end
 
 end
