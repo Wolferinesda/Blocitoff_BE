b7bdef1a000cc1415eeadac91c42e88d730b65e1
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-04-05 17:04:38 -0400

    Changed distance in time to +7 days instead of -7 days

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,7 +1,7 @@
 module ItemsHelper
 
   def days_left(item)
-    distance_of_time_in_words(Time.now, item.created_at - 7.days)
+    distance_of_time_in_words(Time.now, item.created_at + 7.days)
   end
 
 end
